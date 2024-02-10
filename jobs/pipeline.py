36e197bfa160517eb83f025de0a92af1c0cd2513
AR2019git <56238313+AR2019git@users.noreply.github.com>
2024-02-10 01:17:57 -0800

    Update pipeline.py

--- jobs/pipeline.py
+++ jobs/pipeline.py
@@ -131,50 +131,6 @@ def run(spark: SparkSession, config: Dict, logger) -> bool:
 
 def mastereferencedata(spark: SparkSession,df: DataFrame) -> bool:
     
-   """df2 = df.withColumn("Country", when(df["Country"] == "KOREA", "KOR")
-                        .when(df["Country"].isin(["US", "United States", "United states of America"]), "USA")
-                        .when(df["Country"] == "France", "FRA")
-                        .otherwise(df["Country"])) \
-            .withColumn("POLICY_HOLDER_GENDER", 
-                        when(df["POLICY_HOLDER_GENDER"].isin(["Male", "0", "M","male"]), "M")
-                        .when(df["POLICY_HOLDER_GENDER"].isin(["Female", "1", "F","female"]), "F")
-                        .otherwise(df["POLICY_HOLDER_GENDER"])) \
-            .withColumn("CURRENCY", 
-                        when(df["CURRENCY"].isin(["US DOLLAR","US Dollar"]), "USD")
-                        .when(df["CURRENCY"].isin(["Korea Won", "KR"]), "KRW")
-                        .when(df["CURRENCY"].isin(["Australian Dollar", "Aussie Dollar"]), "AUD")
-                        .when(df["CURRENCY"].isin(["Indian Rupee", "Rupee"]), "INR")
-                        .otherwise(df["CURRENCY"])) \
-            .withColumn("PROD_CODE", 
-                        when(df["PROD_CODE"].isin(["TL", "Term", "Life","TERM","TERM LIFE"]), "Term Life")
-                        .when(df["PROD_CODE"].isin(["UL", "Universal"]), "Universal Life")
-                        .when(df["PROD_CODE"].isin(["WL", "Whole","LIFE"]), "Whole Life")
-                        .otherwise(df["PROD_CODE"])) \
-            .withColumn("TRANS_CODE", 
-                        when(df["TRANS_CODE"].isin(["PREMIUM PAYMENT", "PREM", "PRM"]), "PEX")
-                        .when(df["TRANS_CODE"].isin(["CLAIMS PAYMENT", "CLM", "CLAIMS"]), "LDD")
-                        .otherwise(df["TRANS_CODE"])) \
-            .withColumn("POLICY_STATUS", 
-                        when(df["POLICY_STATUS"].isin(["ACTIVE", "In force","Active","0"]), "A")
-                        .when(df["POLICY_STATUS"].isin(["Cancellation", "Terminated","INFORCE","1"]), "I")
-                        .otherwise(df["POLICY_STATUS"]))
-
-
-    return df2
    
-    expected_data = [
-    {
-        "Country": "KOR",
-        "POLICY_HOLDER_GENDER": "M",
-        "CURRENCY": "KRW",
-        "PROD_CODE": "Term Life",
-        "TRANS_CODE": "PEX",
-        "POLICY_STATUS": "A"
-    }]
-    
-    print('in test_pipeline.py --> test_run_sample')
-    expected_df =spark.createDataFrame(expected_data)
-     """
-    
     return True
    
